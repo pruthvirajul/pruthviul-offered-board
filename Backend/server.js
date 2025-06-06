0c62e8c7db7e1a25ae7ea745bc2e007f1289eecc
kvenkat9889@gmail.com <kvenkatesh1294@gmail.com>
2025-06-06 18:06:42 +0530

    offboarding

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3332;
 
 // PostgreSQL connection
 const pool = new Pool({
