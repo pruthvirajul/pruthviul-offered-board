e31276564314f223d3cc8f7b8a021adc4cc954e6
project-3 <your-github-pruthvirajul2001@gmail.com@example.com>
2025-07-09 11:48:52 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3332;
+const port = process.env.PORT || 3680;
 
 // PostgreSQL connection
 const pool = new Pool({
